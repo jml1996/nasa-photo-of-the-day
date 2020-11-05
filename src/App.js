e83310bbeba83b4cf7da55ea32509e3f1f71ea6c
Joshua Lovins <joshualovins@Joshuas-MBP.lan>
2020-11-04 20:07:35 -0500

    Late start. Starting now.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import "./App.css";
 
 function App() {
+
+  useEffect(() => {
+    const fetchPhoto = () => {
+      axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=ltBWr9lWRC3A2m6I2h1FE9eHwjy0ErNe22WGa27D`)
+        .then((res) => console.log(res.data))
+        .catch((err) => debugger);
+    }
+    fetchPhoto();
+  }, []);
+
   return (
     <div className="App">
       <p>
